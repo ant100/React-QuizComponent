496d52335197e0b516a67197eea7dfa0a315408d
Antuanette Loo <antuanette.l@scopicsoftware.com>
2020-11-06 11:41:15 -0500

    display quiz questions and handle events

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,14 +11,24 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 };
     };
 
+    showNextQuestion() {
+        this.setState({quiz_position: this.state.quiz_position + 1});
+    }
+
     render() {
         const isQuizEnd = () => {
-            return this.state.quiz_position - 1 == quizData.quiz_questions.length;
+            return this.state.quiz_position - 1 === quizData.quiz_questions.length;
         }
 
         return (
             <div>
-                { isQuizEnd() ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> 
+                { isQuizEnd() ? 
+                    <QuizEnd /> 
+                    : 
+                    <QuizQuestion
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)} 
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                        /> 
                 }
                 
             </div>
